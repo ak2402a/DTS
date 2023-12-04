717bccc257ec23a69a71c0381a218e716452a4db
Mehdi Daoudi <md8898a@student.american.edu>
2023-12-04 15:32:47 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,44 +1,47 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons'; // assuming you are using Expo, or you can use any other package for icons
+import { Ionicons } from '@expo/vector-icons';
 
 import HomePage from './src/components/HomePage';
 import SchedulingPage from './src/components/SchedulingPage';
 import SettingsPage from './src/components/SettingsPage';
-import SurveyModal from './src/components/SurveyModal';
+import { TasksProvider } from './src/components/TasksContext'; 
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+    <TasksProvider> {
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
 
-            if (route.name === 'Home') {
-              iconName = focused ? 'home' : 'home-outline'; 
-            } else if (route.name === 'Scheduling') {
-              iconName = focused ? 'calendar' : 'calendar-outline'
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'settings' : 'settings-outline';
-            }
+              if (route.name === 'Home') {
+                iconName = focused ? 'home' : 'home-outline'; 
+              } else if (route.name === 'Scheduling') {
+                iconName = focused ? 'calendar' : 'calendar-outline';
+              } else if (route.name === 'Settings') {
+                iconName = focused ? 'settings' : 'settings-outline';
+              }
 
-            // Return the appropriate icon
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'blue',
-          inactiveTintColor: 'gray',
-        }}
-      >
-        <Tab.Screen name="Home" component={HomePage} />
-        <Tab.Screen name="Scheduling" component={SchedulingPage} />
-        <Tab.Screen name="Settings" component={SettingsPage} />
-      </Tab.Navigator>
-    </NavigationContainer>
+              // Return the appropriate icon
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: 'blue',
+            inactiveTintColor: 'gray',
+          }}
+        >
+          <Tab.Screen name="Home" component={HomePage} />
+          <Tab.Screen name="Scheduling" component={SchedulingPage} />
+          <Tab.Screen name="Settings" component={SettingsPage} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    }
+    </TasksProvider>
   );
 }
